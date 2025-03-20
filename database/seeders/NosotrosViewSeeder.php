bc89abc8a4d3d6ea713754808cd7ccfb1320364f
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-03-20 16:06:21 -0500

    final

--- database/seeders/NosotrosViewSeeder.php
+++ database/seeders/NosotrosViewSeeder.php
@@ -16,22 +16,20 @@ class NosotrosViewSeeder extends Seeder
         NosotrosView::updateOrCreate([
             'id' => 1
         ],[
-            'title1section' => 'Todo lo que debes saber sobre nosotros',
+            'title1section' => 'Nuestra historia, nuestro nacimiento',
             'description1section' => 'Nullam nec orci dui. Praesent tristique facilisis quam, a egestas lorem consectetur fringilla. Suspendisse cursus erat eget ante auctor, non hendrerit ipsum egestas. Nullam nec orci dui. Praesent tristique facilisis quam, a egestas lorem consectetur fringilla. Suspendisse cursus erat eget ante auctor, non hendrerit ipsum egestas.',
-            'title2section' => 'La historia de nuestro nacimiento',
-            'description2section' => 'Pellentesque accumsan mauris tincidunt odio elementum imperdiet.',
+            'url_image2section' => '',
 
-            'description3section' => '<p>Nam id eleifend ligula. Nunc ut lobortis nulla, ac congue justo. Nulla tempor varius quam. In lacus elit, convallis vitae orci vitae, ultrices accumsan diam. Etiam nec risus quis velit varius lobortis. Curabitur semper auctor posuere. In elementum placerat quam. Suspendisse mollis luctus suscipit.</p>
-            <p>Integer aliquet mi ex, in iaculis ligula interdum a. Nullam sed ipsum sit amet massa bibendum viverra. Pellentesque accumsan mauris tincidunt odio elementum imperdiet. Nam fringilla scelerisque ipsum non condimentum. Aenean ornare dui sed magna sodales lobortis. Vestibulum porttitor augue vel arcu commodo, sed vulputate turpis fermentum. Nunc nec purus eu purus congue tincidunt in id quam.</p>
-            <p>Curabitur sit amet venenatis felis. Nunc elementum pulvinar sapien, a hendrerit nibh ullamcorper ullamcorper. Nunc sit amet aliquet nunc. Maecenas nec finibus orci. In sodales venenatis dictum. Quisque sit amet ipsum turpis. Nam lobortis justo at magna volutpat, pellentesque suscipit eros commodo. In hac habitasse platea dictumst. Etiam commodo tellus et commodo dapibus.</p>',
             
-            'description4section' => '<p>Nam id eleifend ligula. Nunc ut lobortis nulla, ac congue justo. Nulla tempor varius quam. In lacus elit, convallis vitae orci vitae, ultrices accumsan diam. Etiam nec risus quis velit varius lobortis. Curabitur semper auctor posuere. In elementum placerat quam. Suspendisse mollis luctus suscipit.</p>
-            <p>Integer aliquet mi ex, in iaculis ligula interdum a. Nullam sed ipsum sit amet massa bibendum viverra. Pellentesque accumsan mauris tincidunt odio elementum imperdiet. Nam fringilla scelerisque ipsum non condimentum. Aenean ornare dui sed magna sodales lobortis. Vestibulum porttitor augue vel arcu commodo, sed vulputate turpis fermentum. Nunc nec purus eu purus congue tincidunt in id quam.</p>
-            <p>Curabitur sit amet venenatis felis. Nunc elementum pulvinar sapien, a hendrerit nibh ullamcorper ullamcorper. Nunc sit amet aliquet nunc. Maecenas nec finibus orci. In sodales venenatis dictum. Quisque sit amet ipsum turpis. Nam lobortis justo at magna volutpat, pellentesque suscipit eros commodo. In hac habitasse platea dictumst. Etiam commodo tellus et commodo dapibus.</p>',
-            
-            'title4section' => 'Suscríbete a nuestro blog',
-            'description4section' => 'Sea el primero en conocer los lanzamientos y las novedades y perspectivas de la industria.', 
-            'footer4section' => 'Nos preocupamos por tus datos en nuestra política de privacidad', 
+            'title3section' => 'Nuestras motivaciones diarias',
+            'url_image4section' => '',
+
+            'title3secondsection' => 'Misión',
+            'description3secondsection' => 'Conectamos dispositivos a la nube. Desarrollamos e integramos soluciones basadas en el Internet de las cosas (Internet of Things - IoT) para proyectos de Smart Grid a la medida. Utilizamos protocolos de comunicación que permiten la escabilidad de nuestras soluciones y flexibilidad para la integración a diferentes industrias. Energía inteligente.' ,
+
+            'title4section' => 'Visión',
+            'description4section' => 'Software de administración de energía, ofrece un conjunto de soluciones: desde facturación, monitoreo, eficiencia energética y respuesta a la demanda hasta análisis exhaustivos de calidad de energía y soporte de procesos centrales de planificación y previsión para generadores, transmisores y distribuidores de energía e también para grandes proyectos residenciales, edificios de oficinas, centros comerciales, entre otros.' ,
+
         ]);
     }
 }
